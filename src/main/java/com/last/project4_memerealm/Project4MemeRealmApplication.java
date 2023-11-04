6717a12a8c48bed3f8094b42e03d6328fb3e973e
tuangadmm <t2321502792@gmail.com>
2023-11-04 16:54:59 +0700

    iniy

---
+++ src/main/java/com/last/project4_memerealm/Project4MemeRealmApplication.java
@@ -0,0 +1,13 @@
+package com.last.project4_memerealm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project4MemeRealmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project4MemeRealmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/last/project4_memerealm/Project4MemeRealmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.last.project4_memerealm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project4MemeRealmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
